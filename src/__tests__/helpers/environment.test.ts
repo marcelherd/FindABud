07780913a2d3b3179d404e4c0f8e43d515e5ca64
Marcel Herd <me@marcelherd.com>
2022-04-10 17:13:36 +0200

    Fix typo in environment.test.ts

--- src/__tests__/helpers/environment.test.ts
+++ src/__tests__/helpers/environment.test.ts
@@ -2,23 +2,23 @@ import { getEnvironmentVariable } from '@helpers/environment';
 
 describe('getEnvironmentVariable', () => {
   it('should return the value of the environment variable if it exists', () => {
-    const foo = getEnvironmentVariable('foo');
+    const foo = getEnvironmentVariable('FOO');
     expect(foo).toBe<string>('bar');
   });
 
   it('should return the value of the environment variable if it exists and a default value is provided', () => {
-    const foo = getEnvironmentVariable('foo', 'defaultValue');
+    const foo = getEnvironmentVariable('FOO', 'defaultValue');
     expect(foo).toBe<string>('bar');
   });
 
   it('should return the default value if the environment variable does not exist', () => {
-    const bar = getEnvironmentVariable('bar', 'defaultValue');
+    const bar = getEnvironmentVariable('BAR', 'defaultValue');
     expect(bar).toBe<string>('defaultValue');
   });
 
   it('should throw an error if the environment variable does not exist and no default value is provided', () => {
-    expect(() => getEnvironmentVariable('bar')).toThrow(
-      'Required environment variable not set: bar'
+    expect(() => getEnvironmentVariable('BAR')).toThrow(
+      'Required environment variable not set: BAR'
     );
   });
 });
